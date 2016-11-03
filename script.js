c06de368d6e6fd423ca1775b435d03fe10c4c621
John Doe <webdevnorth@gmail.com>
2016-11-03 18:33:48 +0000

    decreased margin-top on container

--- script.js
+++ script.js
@@ -9,7 +9,7 @@
 window.addEventListener('scroll', function() {
 	// check if scroll position has passed a certain point
 
-	if (window.pageYOffset > 200) {
+	if (window.pageYOffset > 100) {
 		document.querySelector('.custom-nav').classList.add('custom-nav-fixed');
 	} else {
 		document.querySelector('.custom-nav').classList.remove('custom-nav-fixed');
